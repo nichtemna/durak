bebaabaa426da8e4c196f7676bb030606f6629a8
lshyshova <lina.shyshova@booking.com>
2022-02-10 19:37:38 +0100

    Add Attacker and Defender

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,7 +1,7 @@
 class Game {
 
-    val player1 = BasePlayer()
-    val player2 = BasePlayer()
+    val player1 = BasePlayer(1)
+    val player2 = BasePlayer(2)
 
     val deck = Deck(getDeck())
 
@@ -13,9 +13,29 @@ class Game {
         player1.takeCards(deck.take(2))
         player2.takeCards(deck.take(2))
         deck.takeTrump()
+
+        player1.printCards()
+        player2.printCards()
+        println("Trump ${deck.trump?.suit} ${deck.trump?.value}")
+
+        while (true) {
+            val tableCards = mutableListOf<Card>()
+
+            while (true) {
+                val move = player1.nextMove()
+
+                when (move) {
+                    is AttackerMove.NextCards -> {
+                        tableCards.add(move.card)
+                    }
+                }
+            }
+
+            if (deck.cards.isEmpty() && (player1.cards.isEmpty() || player2.cards.isEmpty())) {
+                break;
+            }
+        }
+
+        println("Winnar is ")
     }
-
-
-
-
 }
--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,29 +1,59 @@
 interface Player {
 
-    fun nextMove()
+    fun nextMove(cardsOnTable: List<Card>): AttackerMove
 
-
+    fun defend(card: Card): DefenderMove
 }
 
-class BasePlayer() : Player{
+class BasePlayer(val id: Int) : Player {
     val cards = mutableListOf<Card>()
 
     fun takeCards(newCards: List<Card>) {
         cards.addAll(newCards)
     }
 
-    override fun nextMove() {
+    override fun nextMove(cardsOnTable: List<Card>): AttackerMove {
+        return if (cards.isNotEmpty()) {
+            if (cardsOnTable.isEmpty()) {
+                AttackerMove.NextCards(cards.first())
+            } else {
+                val nextCard = getNextMove(cardsOnTable, cards)
+
+                if (nextCard == null) {
+                    AttackerMove.Stop
+                } else {
+                    AttackerMove.NextCards(nextCard)
+                }
+            }
+        } else {
+            AttackerMove.Stop
+        }
+    }
+
+    private fun getNextMove(cardsOnTable: List<Card>, cards: MutableList<Card>): Card? {
+        return cardsOnTable.flatMap { card -> cards.filter { card.value == it.value } }.firstOrNull()
+    }
+
+    override fun defend(card: Card): DefenderMove {
 
     }
 
+    fun printCards() {
+        println("User $id has cards:")
+        cards.forEach {
+            print("${it.suit} ${it.value} | ")
+        }
+        println()
+    }
 }
 
 
-sealed class Move {
+sealed class AttackerMove {
+    data class NextCards(val card: Card) : AttackerMove()
+    object Stop : AttackerMove()
+}
 
-    // next card
-
-    // stop
-    // take
-
+sealed class DefenderMove {
+    data class Cover(val card: Card) : DefenderMove()
+    data class Take(val cards: List<Card>) : DefenderMove()
 }
